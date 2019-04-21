ef3684f67783feb92892536de62090b666cecadb
Alan Montoya <alanmontoya2010@att.net>
2019-04-20 19:36:34 -0500

    firebase configuration added to js file

---
+++ assets/javascript/moment.js
@@ -0,0 +1,15 @@
+//CODING FOR TRAIN SCHEDULER HOMEWORK
+
+
+
+//CONFIGURE FIREBASE 
+var config = {
+    apiKey: "AIzaSyA1rVv53p72dWWUtyMD4y88OzgOGbThHEI",
+    authDomain: "codingtrain-87199.firebaseapp.com",
+    databaseURL: "https://codingtrain-87199.firebaseio.com",
+    projectId: "codingtrain-87199",
+    storageBucket: "codingtrain-87199.appspot.com",
+    messagingSenderId: "574744011986"
+  };
+  
+  firebase.initializeApp(config);
